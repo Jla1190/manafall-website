b803b5a0be161891579c634dc87591b53411b158
Jla1190 <jla1190@users.noreply.github.com>
2026-09-20 19:30:31 -0500

    Ship Windows 0.1.8 Mega download and notes-only patch-manifest.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.MANAFALL_SITE = {
   kofiUrl: "https://ko-fi.com/manafall",
-  downloadUrl: "https://mega.nz/file/nM0GlRTJ#ONbmjnmZIyFvGM4igMLrg0AGbaiiQf8FY77t7R70huE",
+  downloadUrl: "https://mega.nz/file/7B9GVR7Q#86R7NRzK4dYDKpnsaAA0O9lwpTfbLOGd0Dc74gOBsjw",
   downloadLabel: "Download for Windows",
   creatorName: "Jean-Luc Alexander",
   discordUrl: "https://discord.gg/CH8GjWaDnx",
