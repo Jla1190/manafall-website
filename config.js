be045fb74afd4ef24e7aea3a643774a6b550d9da
Jean-Luc Alexander <jeanluc@manafall.local>
2026-09-22 08:37:15 -0500

    Ship Windows client 0.1.13 and patch notes.

--- config.js
+++ config.js
@@ -1,12 +1,14 @@
-﻿window.MANAFALL_SITE = {
+window.MANAFALL_SITE = {
   kofiUrl: "https://ko-fi.com/manafall",
-  downloadUrl: "https://mega.nz/file/rAEkyDwT#KTykmjHo5Keu0UMeIhP0RRHR1RtSKB0aqc8k8tuQJU4",
+  downloadUrl: "https://mega.nz/file/zZMTzAhb#Gql2tsWUs3tJZ5j7lF65Gi6-mxLWON_47x6lJS0rFKQ",
   downloadLabel: "Download for Windows",
   creatorName: "Jean-Luc Alexander",
   discordUrl: "https://discord.gg/CH8GjWaDnx",
   itchUrl: "https://orangejuice90.itch.io/manafall",
   trailerUrl: "https://youtube.com/shorts/TudrqM7-3_A",
   githubRepo: "Jla1190/manafall-website",
+  counterApiBase: "https://abacus.jasoncameron.dev",
+  counterNamespace: "manafall",
   ageRating: "Teen",
   ageMinimum: "13+",
   ageDescriptors: "Fantasy Violence, Mild Blood, Mild Drug Use",
