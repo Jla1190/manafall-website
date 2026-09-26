f2f09faa0e0372a479ce5272f7ba03838514196a
Jean-Luc Alexander <cleanwrapjeanluc@yahoo.com>
2026-09-26 10:34:26 -0500

    Update Android APK Mega link (P1+Clerk touch UI android-20260926-1034)

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.MANAFALL_SITE = {
   kofiUrl: "https://ko-fi.com/manafall",
   downloadUrl: "https://mega.nz/file/Hd8n3LKb#mvqzUMhjnnLmVwXAzZe67t3_5taRKgEF-L1IWXPRuOc",
   downloadLabel: "Download for Windows",
-  androidDownloadUrl: "https://mega.nz/file/3AtGkZrR#EyCmIQCvSd_o0PmHSWHDCClMvIz9f2OYRTQZaQP8HGU",
+  androidDownloadUrl: "https://mega.nz/file/mBVTjZhA#hFQigSfQN6P2WNaYBQcMW8OXDxmFZjJXO4dW5HTFJ20",
   androidDownloadLabel: "Download for Android",
   creatorName: "Jean-Luc Alexander",
   discordUrl: "https://discord.gg/CH8GjWaDnx",
